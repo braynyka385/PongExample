8d063e896cb4d31f3d15a8e379b1286960f382e1
Limitlez26 <syntaxerror212@gmail.com>
2021-01-06 10:41:13 -0500

    done

--- PongExample/Form1.cs
+++ PongExample/Form1.cs
@@ -16,7 +16,7 @@ namespace PongExample
         int paddle1Y = 170;
         int player1Score = 0;
 
-        int paddle2X = 580;
+        int paddle2X = 50;
         int paddle2Y = 170;
         int player2Score = 0;
 
@@ -26,18 +26,22 @@ namespace PongExample
 
         int ballX = 295;
         int ballY = 195;
-        int ballXSpeed = 6;
-        int ballYSpeed = -6;
+        float ballXSpeed = 0;
+        float ballYSpeed = 0;
         int ballWidth = 10;
         int ballHeight = 10;
 
-        bool wDown = false;
-        bool sDown = false;
-        bool upArrowDown = false;
-        bool downArrowDown = false;
+        bool playStart = true;
+
+        bool[] pressedKeys = new bool[10];
+
+        float accel = 1.05f;
+
+        bool activePlayer = true; //true == p1, false == p2
 
         SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
         SolidBrush whiteBrush = new SolidBrush(Color.White);
+        Pen bluePen = new Pen(Color.Blue, 2);
         Font screenFont = new Font("Consolas", 12);
 
 
@@ -51,16 +55,28 @@ namespace PongExample
             switch (e.KeyCode)
             {
                 case Keys.W:
-                    wDown = true;
+                    pressedKeys[0] = true;
                     break;
                 case Keys.S:
-                    sDown = true;
+                    pressedKeys[1] = true;
+                    break;
+                case Keys.A:
+                    pressedKeys[2] = true;
+                    break;
+                case Keys.D:
+                    pressedKeys[3] = true;
                     break;
                 case Keys.Up:
-                    upArrowDown = true;
+                    pressedKeys[4] = true;
                     break;
                 case Keys.Down:
-                    downArrowDown = true;
+                    pressedKeys[5] = true;
+                    break;
+                case Keys.Left:
+                    pressedKeys[6] = true;
+                    break;
+                case Keys.Right:
+                    pressedKeys[7] = true;
                     break;
             }
 
@@ -71,16 +87,28 @@ namespace PongExample
             switch (e.KeyCode)
             {
                 case Keys.W:
-                    wDown = false;
+                    pressedKeys[0] = false;
                     break;
                 case Keys.S:
-                    sDown = false;
+                    pressedKeys[1] = false;
+                    break;
+                case Keys.A:
+                    pressedKeys[2] = false;
+                    break;
+                case Keys.D:
+                    pressedKeys[3] = false;
                     break;
                 case Keys.Up:
-                    upArrowDown = false;
+                    pressedKeys[4] = false;
                     break;
                 case Keys.Down:
-                    downArrowDown = false;
+                    pressedKeys[5] = false;
+                    break;
+                case Keys.Left:
+                    pressedKeys[6] = false;
+                    break;
+                case Keys.Right:
+                    pressedKeys[7] = false;
                     break;
             }
 
@@ -89,35 +117,60 @@ namespace PongExample
         private void gameTimer_Tick(object sender, EventArgs e)
         {
             //move ball
-            ballX += ballXSpeed;
-            ballY += ballYSpeed;
+            ballX += Convert.ToInt32(ballXSpeed); 
+            ballY += Convert.ToInt32(ballYSpeed);
 
             //move player 1
-            if (wDown == true && paddle1Y > 0)
+            if (pressedKeys[0] == true && paddle1Y > 0)
             {
                 paddle1Y -= paddleSpeed;
             }
 
-            if (sDown == true && paddle1Y < this.Height - paddleHeight)
+            if (pressedKeys[1] == true && paddle1Y < this.Height - paddleHeight)
             {
                 paddle1Y += paddleSpeed;
             }
 
+            if (pressedKeys[2] == true && paddle1X > 0 + paddleWidth) // && paddle1X > this.Width + paddleWidth
+            {
+                paddle1X -= paddleSpeed;
+            }
+
+            if (pressedKeys[3] == true && paddle1X < this.Width - paddleWidth)
+            {
+                paddle1X += paddleSpeed;
+            }
+
             //move player 2
-            if (upArrowDown == true && paddle2Y > 0)
+            if (pressedKeys[4] == true && paddle2Y > 0)
             {
                 paddle2Y -= paddleSpeed;
             }
 
-            if (downArrowDown == true && paddle2Y < this.Height - paddleHeight)
+            if (pressedKeys[5] == true && paddle2Y < this.Height - paddleHeight)
             {
                 paddle2Y += paddleSpeed;
+            }
+            if (pressedKeys[6] == true && paddle2X > 0 + paddleWidth) // && paddle1X > this.Width + paddleWidth
+            {
+                paddle2X -= paddleSpeed;
+            }
+
+            if (pressedKeys[7] == true && paddle2X < this.Width - paddleWidth)
+            {
+                paddle2X += paddleSpeed;
             }
 
             //top and bottom wall collision
             if (ballY < 0 || ballY > this.Height - ballHeight)
             {
-                ballYSpeed *= -1;  // or: ballYSpeed = -ballYSpeed;
+                ballYSpeed *= -1 * accel;  // or: ballYSpeed = -ballYSpeed;
+            }
+
+            //back wall collision
+            if (ballX > this.Width - ballWidth)
+            {
+                ballXSpeed *= -1 * accel;
             }
 
             //create Rectangles of objects on screen to be used for collision detection
@@ -127,35 +180,101 @@ namespace PongExample
 
             //check if ball hits either paddle. If it does change the direction
             //and place the ball in front of the paddle hit
-            if (player1Rec.IntersectsWith(ballRec))
+            if (player1Rec.IntersectsWith(ballRec) && activePlayer == true)
             {
-                ballXSpeed *= -1;
-                ballX = paddle1X + paddleWidth + 1;
+                if (playStart == true)
+                {
+                    if (pressedKeys[0] == true)
+                    {
+                        ballXSpeed = 6;
+                        ballYSpeed = -6;
+                    }
+                    else if(pressedKeys[1] == true)
+                    {
+                        ballXSpeed = 6;
+                        ballYSpeed = 6;
+                    }
+                    else
+                    {
+                        ballXSpeed = 6;
+                    }
+                    playStart = false;
+                }
+                else
+                {
+                    ballXSpeed *= -1 * accel;
+                    ballX = paddle1X + paddleWidth + 1;
+                    if (pressedKeys[0] == true)
+                    {
+                        ballYSpeed -= 0.15f;
+                    }
+                    else if (pressedKeys[1] == true)
+                    {
+                        ballYSpeed += 0.15f;
+                    }
+                }
+                
+                
+                activePlayer = false;
             }
-            else if (player2Rec.IntersectsWith(ballRec))
+            else if (player2Rec.IntersectsWith(ballRec) && activePlayer == false)
             {
-                ballXSpeed *= -1;
-                ballX = paddle2X - ballWidth - 1;
+                if (playStart == true)
+                {
+                    if (pressedKeys[4] == true)
+                    {
+                        ballXSpeed = 6;
+                        ballYSpeed = -6;
+                    }
+                    else if (pressedKeys[5] == true)
+                    {
+                        ballXSpeed = 6;
+                        ballYSpeed = 6;
+                    }
+                    else
+                    {
+                        ballXSpeed = 6;
+                    }
+                    playStart = false;
+                }
+                else
+                {
+                    ballXSpeed *= -1 * accel;
+                    ballX = paddle2X - ballWidth - 1;
+
+                    if (pressedKeys[4] == true)
+                    {
+                        ballYSpeed -= 0.15f;
+                    }
+                    else if (pressedKeys[5] == true)
+                    {
+                        ballYSpeed += 0.15f;
+                    }
+                }
+                
+                activePlayer = true;
             }
 
             if (ballX < 0)
             {
-                player2Score++;
+                if (activePlayer == true)
+                {
+                    player1Score++;
+                }
+                else
+                {
+                    player2Score++;
+                }
+                
                 ballX = 295;
                 ballY = 195;
 
-                paddle1Y = 170;
-                paddle2Y = 170;
-            }
-            else if (ballX > 600)
-            {
-                player1Score++;
-
-                ballX = 295;
-                ballY = 195;
+                ballXSpeed = 0;
+                ballYSpeed = 0;
 
                 paddle1Y = 170;
                 paddle2Y = 170;
+                playStart = true;
             }
 
             if (player1Score == 3 || player2Score == 3)
@@ -173,7 +292,15 @@ namespace PongExample
 
             e.Graphics.FillRectangle(blueBrush, paddle1X, paddle1Y, paddleWidth, paddleHeight);
             e.Graphics.FillRectangle(blueBrush, paddle2X, paddle2Y, paddleWidth, paddleHeight);
-            
+
+            if (activePlayer == true)
+            {
+                e.Graphics.DrawRectangle(bluePen, paddle1X, paddle1Y, paddleWidth, paddleHeight);
+            }
+            else
+            {
+                e.Graphics.DrawRectangle(bluePen, paddle2X, paddle2Y, paddleWidth, paddleHeight);
+            }
 
             e.Graphics.DrawString($"{player1Score}", screenFont, whiteBrush, 280, 10);
             e.Graphics.DrawString($"{player2Score}", screenFont, whiteBrush, 310, 10);
